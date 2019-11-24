164e734542bcec4acd3437f0334572c7559abd92
sudiomary <kekovasudi.gmail.com>
2019-11-25 01:09:45 +0300

    My project setup completed successfully

---
+++ sudishoppingbackend/src/main/java/net/sudi/sudishoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sudi.sudishoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
